0d489bef248c7307a24bd4520611dc03a989986b
lucas-nasc <lcsnascimento91@gmail.com>
2024-01-08 14:15:36 -0300

    Desafio concluido

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+System.Console.WriteLine("Smartphone Iphone");
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new("123456","Modelo1","152434526",64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new("157896526","Modelo2","2222222",128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+
